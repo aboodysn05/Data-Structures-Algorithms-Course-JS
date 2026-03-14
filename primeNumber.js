4d3c04c63229c9af084fe47ad7ef9863ce289a21
Abdullah Yaseen <abdullahanin75@gmail.com>
2026-03-14 20:31:27 +0300

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -17,3 +17,23 @@ console.log(isPrime(4)) // false
 console.log(isPrime(5)) // true
 
 // Big-O = O(n)
+
+function betterIsPrime(n) {
+  if(n < 2) {
+    return false
+  } else {
+    for(let i = 2; i < Math.sqrt(n); i++) {
+      if(n % i === 0) {
+        return false
+      } else {
+        return true
+      }
+    }
+  }
+}
+
+console.log(betterIsPrime(0)) // false
+console.log(betterIsPrime(4)) // false
+console.log(betterIsPrime(5)) // true
+
+// Big-O = O(sqrt(n))
