066c574cc7994c0a152310b3115d57db140f5c7e
Abdullah Yaseen <abdullahanin75@gmail.com>
2026-03-14 20:25:58 +0300

    primeNumber.js

---
+++ primeNumber.js
@@ -0,0 +1,17 @@
+function isPrime(n) {
+  if(n < 2) {
+    return false
+  } else {
+    for(let i = 2; i < n; i++) {
+      if(n % i === 0) {
+        return false
+      } else {
+        return true
+      }
+    }
+  }
+}
+
+console.log(isPrime(0)) // false
+console.log(isPrime(4)) // false
+console.log(isPrime(5)) // true
